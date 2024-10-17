287b95a6f9238e2180d875e2e908bfc76358a838
Abu Jehoiada <abujehoiada@gmail.com>
2024-10-17 08:34:36 +0000

    Fixed code

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 --  ranks country origins of bands
 -- ordered by the number of (non-unique) fans
-SOURCE ./metal_bands.sql
-SELECT origin, COUNT(*) AS nb_fans
+
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
